34f367cd378fc228118911ab49d9669611ba5d47
AdhilKappil <adhilalikappil@gmail.com>
2024-07-07 15:10:42 +0530

    feat:about user

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+function About() {
+  return (
+    <div className="p-5">
+      <div className="">
+        <p className="text-sm font-thin">
+          A self-taught MERN Stack Developer focused on writing clean, eﬃcient
+          code. Proficient in JavaScript, React.js, Node.js, Express.js and
+          MongoDB. Experienced in all development phases, from design to
+          deployment of project. Committed to ongoing learning and staying
+          current with industry trends
+        </p>
+      </div>
+      <div>
+        <p className="font-semibold text-xl mt-5">Education</p>
+        <p className="text-sm font-medium mt-2">University of Calicut</p>
+        <p className="text-sm">Bachelor of Commerce - BCom, Computer Programming</p>
+         <p className="text-sm">2018 - 2021</p>   
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,7 +49,7 @@ function Home() {
              <div>
               {selectedTab === "about" ? <About/> 
               :
-              } 
+              ""} 
              </div>
             </div>
            </div>
